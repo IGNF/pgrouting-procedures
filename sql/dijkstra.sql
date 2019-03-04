7b37a4762ee708eb5b0343dae89dce1ece46e9ce
amaury.zarzelli <amaury.zarzelli@ign.fr>
2019-03-04 16:27:23 +0100

    Update

--- sql/dijkstra.sql
+++ sql/dijkstra.sql
@@ -1,12 +1,10 @@
 -- Point du graphe le plus proche d'un couple de cordonnées
-CREATE OR REPLACE FUNCTION nearest_node(lat double precision, lon double precision) RETURNS bigint AS $$
+CREATE OR REPLACE FUNCTION nearest_node(lat1 double precision, lon1 double precision) RETURNS bigint AS $$
   SELECT id
   FROM ways_vertices_pgr
-  ORDER BY the_geom <-> st_setsrid(st_makepoint(lon,lat),4326)
+  ORDER BY the_geom <-> st_setsrid(st_makepoint(lon1,lat1),4326)
   LIMIT 1
 $$ LANGUAGE SQL ;
-
-
 
 -- Dijskstra entre lat1 lon1 et lat2 lon2
 CREATE OR REPLACE FUNCTION coord_dijkstra(lat1 double precision,
@@ -24,15 +22,15 @@ CREATE OR REPLACE FUNCTION coord_dijkstra(lat1 double precision,
     agg_cost double precision,
     geom_json text
     ) AS $$
-  SELECT path.*, ST_AsGeoJSON(ways.the_geom) FROM pgr_dijkstra(concat('SELECT id,source,target,',
-                                                                       costname,
-                                                                       ' AS cost,',
-                                                                       rcostname,
-                                                                       ' AS reverse_cost FROM ways'
-                                                                       ),
-                                                                nearest_node(lat1,lon1),
-                                                                nearest_node(lat2,lon2))
-                                                  AS path
+  SELECT path.*, ST_AsGeoJSON(ways.the_geom)
+  FROM pgr_dijkstra(concat('SELECT id,source,target,',
+                           costname,
+                           ' AS cost,',
+                           rcostname,
+                           ' AS reverse_cost FROM ways'
+                           ),
+                    nearest_node(lat1,lon1),
+                    nearest_node(lat2,lon2)) AS path
   LEFT JOIN ways ON (path.edge = ways.id)
   ORDER BY seq
 $$ LANGUAGE SQL ;
