9385c618a49ca6a22725d59556461f30e1fdb885
Moez Jilani <moez.jilani@ign.fr>
2019-09-11 15:37:12 +0200

    Commit intermédiaire.

--- sql/isochroneProcedures.sql
+++ sql/isochroneProcedures.sql
@@ -1,62 +1,75 @@
-/* [isochrone] Conversion d'un point de coordonnées (point de départ) en géométrie. */
+-- [isochrone] Conversion d'un point de coordonnées en géométrie.
 CREATE OR REPLACE FUNCTION coordToGeom(startingPoint double precision[]) RETURNS geometry AS $$
   DECLARE
     lon double precision;
     lat double precision;
   BEGIN
-    lon := coordinatesTable[1];
-    lat := coordinatesTable[2];
+    lon := startingPoint[1];
+    lat := startingPoint[2];
 
     RETURN st_setsrid(st_makepoint(lon, lat), 4326);
   END;
-$$ LANGUAGE 'plpgsql' ;
+$$ LANGUAGE 'plpgsql';
 
-/* [isochrone] Converstion d'un point de coordonnées vers un identifiant de vertex. */
+-- [isochrone] Converstion d'un point de coordonnées vers un identifiant de vertex.
 CREATE OR REPLACE FUNCTION startingPointToVID(startingPoint double precision[]) RETURNS integer AS $$
   BEGIN
     RETURN nearest_node(startingPoint[1], startingPoint[2]);
   END ;
-$$ LANGUAGE 'plpgsql' ;
+$$ LANGUAGE 'plpgsql';
 
-/* [isochrone] Génération d'isochrone. */
+-- [isochrone] Génération d'isochrone.
 CREATE OR REPLACE FUNCTION isochroneGenerator(
-  startingPoint double precision [],  /* Point de départ du calcul. */
-  costValue double precision,         /* Valeur du coût. */
-  costName text,                      /* Nom de la colonne du coût. */
-  rcostName text,                     /* Nom de la colonne de coût inverse. */
-  where_clause text                   /* Clause WHERE pour la sélection d'une partie du graphe. */)
-  /* TODO: Add RETURNS TABLE.. */
+  startingPoint double precision [],  -- Point de départ du calcul.
+  costValue double precision,         -- Valeur du coût.
+  costName text,                      -- Nom de la colonne du coût.
+  rcostName text,                     -- Nom de la colonne de coût inverse.
+  where_clause text                   -- Clause WHERE pour la sélection d'une partie du graphe.
+  )
+  RETURNS TABLE (
+    seq integer,            -- Valeur séquentielle (commençant par 1).
+    start_vid bigint,       -- Identifiant du vertex initial (point de départ).
+    node bigint,            -- Identifiant du noeud.
+    edge double precision,  -- Identifiant du edge.
+    cost float              -- Coût du edge parcouru.
+  ) AS $$
   DECLARE
     graph_query text;
     final_query text;
   BEGIN
-    /* Requête permettant de récupèrer le graphe. */
-    graph_query := concat('SELECT id, source, target, ', costName,' AS cost, ', rcostName,' AS reverse_cost FROM ways', where_clause);
+    -- Requête permettant de récupèrer le graphe.
+    -- graph_query = concat('SELECT id, source, target, ', costName,' AS cost, ', rcostName,' AS reverse_cost FROM ways', where_clause);
+    graph_query = concat('SELECT id, source, target, ', costName,' AS cost, ', rcostName,' AS reverse_cost FROM ways');
 
-    /* Création de la requête du calcul d'isochrone. */
-    /* pgr_drivingDistance(edges_sql, start_vid, distance, directed)
-    RETURNS SET OF (seq, node, edge, cost, agg_cost) */
-    final_query := 'SELECT * FROM pgr_drivingDistance($1, startingPointToVID($2), $3)';
+    -- Création de la requête du calcul d'isochrone.
+    final_query = 'SELECT * FROM pgr_drivingDistance($1, startingPointToVID($2), $3)';
 
-    /* Exécution de la requête du calcul d'isochrone. */
+    -- Exécution de la requête du calcul d'isochrone.
     RETURN QUERY EXECUTE final_query
       USING graph_query, startingPoint, costValue;
   END;
-$$ LANGUAGE 'plpgsql' ;
+$$ LANGUAGE 'plpgsql';
 
-/* [isochrone] Point d'entrée de l'API. */
+-- [isochrone] Point d'entrée de l'API.
 CREATE OR REPLACE FUNCTION generateIsochrone(
-  startingPoint double precision [],  /* Point de départ du calcul. */
-  costValue double precision,         /* Valeur du coût. */
-  costColumn text,                    /* Nom de la colonne du coût. */
-  rcostColumn text,                   /* Nom de la colonne de coût inverse. */)
-  /* TODO: Add RETURNS TABLE.. */
+  startingPoint double precision [],  -- Point de départ du calcul.
+  costValue double precision,         -- Valeur du coût.
+  costColumn text,                    -- Nom de la colonne du coût.
+  rcostColumn text                    -- Nom de la colonne de coût inverse.
+  )
+  RETURNS TABLE (
+    seq integer,            -- Valeur séquentielle (commençant par 1).
+    start_vid bigint,       -- Identifiant du vertex initial (point de départ).
+    node bigint,            -- Identifiant du noeud.
+    edge double precision,  -- Identifiant du edge.
+    cost float              -- Coût du edge parcouru.
+  ) AS $$
   DECLARE
     where_clause text;
   BEGIN
-    /* P.S. Le rayon du buffer a été temporairement mis à 10. */
-    where_clause = concat(' WHERE the_geom @ ( SELECT ST_Buffer( coordToGeom(''', startingPoint, ''' ), ', 10, ' ) )');
+    -- P.S. Le rayon du buffer a été temporairement mis à 10.
+    where_clause = concat(' WHERE the_geom && (SELECT ST_Buffer(coordToGeom(''', startingPoint, '''),', 10,'))');
 
     RETURN QUERY SELECT * FROM isochroneGenerator(startingPoint, costValue, costColumn, rcostColumn, where_clause);
-  END ;
-$$ LANGUAGE 'plpgsql' ;
+  END;
+$$ LANGUAGE 'plpgsql';
