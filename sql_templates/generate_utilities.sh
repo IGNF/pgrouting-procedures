bad214883b75079af01bae396b8d612c917d3c57
amaury.zarzelli <amaury.zarzelli@ign.fr>
2019-11-26 10:04:26 +0100

    Fonction de nettoyage de graphe

--- sql_templates/generate_utilities.sh
+++ sql_templates/generate_utilities.sh
@@ -18,4 +18,58 @@ CREATE OR REPLACE FUNCTION $SCHEMA.nearest_node(lon double precision, lat double
     RETURN result;
   END ;
 \$\$ LANGUAGE 'plpgsql';
+
+
+-- nettoie le graphe : donne un coût négatif aux arc isolés par rapport à la composante connexe principale
+
+CREATE OR REPLACE FUNCTION $SCHEMA.clean_graph(
+    profile_name text -- Nom du profil à nettoyer
+  )
+  RETURNS void AS \$\$
+  DECLARE
+    isolated integer[];
+    node_id integer;
+    connected_component_query text;
+    update_query text;
+  BEGIN
+    connected_component_query := concat(
+      'SELECT id,
+      source,
+      target,
+      cost_s_', profile_name,' as cost,
+      reverse_cost_s_', profile_name,' as reverse_cost
+      from $SCHEMA.ways'
+    );
+
+    isolated := ARRAY(
+      WITH biggest_component AS
+        (
+          SELECT component AS name, count(*) AS nb
+          FROM
+            pgr_connectedComponents(connected_component_query)
+          GROUP BY component
+          ORDER BY nb DESC
+          LIMIT 1
+        )
+      SELECT node
+      FROM
+        pgr_connectedComponents(
+          connected_component_query
+        ),
+        biggest_component
+      WHERE
+      component != biggest_component.name
+    );
+
+  update_query := concat('UPDATE $SCHEMA.ways
+    SET cost_s_', profile_name,' = -1,
+      cost_m_', profile_name,' = -1,
+      reverse_cost_s_', profile_name,' = -1,
+      reverse_cost_m_', profile_name,' = -1
+    WHERE $SCHEMA.ways.target = ANY(''', isolated, ''') OR $SCHEMA.ways.source = ANY(''', isolated, ''');'
+  );
+  EXECUTE update_query;
+  END;
+\$\$ LANGUAGE 'plpgsql' ;
+
 EOF
