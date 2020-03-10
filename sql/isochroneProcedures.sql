6f23010e9b50431a9a3ae01a551d467fd5813d4e
amaury.zarzelli <amaury.zarzelli@ign.fr>
2020-03-10 13:58:29 +0100

    Meilleur rattachement au graphe

--- sql/isochroneProcedures.sql
+++ sql/isochroneProcedures.sql
@@ -11,16 +11,6 @@ CREATE OR REPLACE FUNCTION coordToGeom(location double precision[]) RETURNS geom
   END;
 $$ LANGUAGE 'plpgsql' ;
 
--- Converstion d'un point de coordonnées en un identifiant de vertex.
-CREATE OR REPLACE FUNCTION locationToVID(location double precision[],
-                                        costname text,         -- nom de la colonne du coût
-                                        rcostname text,        -- nom de la colonne de coût inverse
-                                        where_clause text) RETURNS integer AS $$
-  BEGIN
-    RETURN nearest_node(location[1], location[2], costname, rcostname, where_clause);
-  END ;
-$$ LANGUAGE 'plpgsql' ;
-
 -- Calcul de l'isochrone et génération de la géométrie.
 CREATE OR REPLACE FUNCTION isochroneGenerator(
   location double precision [],  -- Point de départ/arrivée du calcul.
@@ -37,17 +27,62 @@ CREATE OR REPLACE FUNCTION isochroneGenerator(
     graph_query text;
     isochrone_query text;
     final_query text;
+    temp_fraction float;
+    nedge_id int;
   BEGIN
+
+    -- Point temporaire
+    nedge_id := nearest_edge(location[1], location[2], costname, rcostname, where_clause);
+
+    temp_fraction := ST_LineLocatePoint(
+      (SELECT the_geom FROM ways
+        WHERE id = nedge_id
+      ),
+      st_setsrid(st_makepoint(location[1], location[2]),4326)
+    );
+
+
+    CREATE TEMP TABLE temp_point ON COMMIT DROP AS
+      SELECT
+        -1 as id,
+        ST_LineInterpolatePoint(
+          (SELECT the_geom FROM ways WHERE id = nedge_id),
+          temp_fraction
+        ) as the_geom
+    ;
+
+    -- Arcs temporaires
+    EXECUTE concat('CREATE TEMP TABLE temp_edges ON COMMIT DROP AS
+      -- arc de source vers -1
+      (SELECT
+        -1 as id,
+        source as source,
+        -1 as target,
+        ', temp_fraction, ' * ', costName, ' as ', costName, ',
+        ', temp_fraction, ' * ', rcostName, ' as ', rcostName, '
+      FROM ways
+      WHERE id = ', nedge_id, ')
+      UNION
+      -- arc de -1 vers target
+      (SELECT
+        -2 as id,
+        -1 as source,
+        target as target,
+        (1 - ', temp_fraction, ') * ', costName, ' as ', costName, ',
+        (1 - ', temp_fraction, ') * ', rcostName, ' as ', rcostName, '
+      FROM ways
+      WHERE id = ', nedge_id, ')')
+    ;
+
     -- Requête permettant de récupèrer le graphe.
     IF (direction = 'arrival') THEN
-      graph_query := concat('SELECT id, source, target, ', rcostName,' AS cost, ', costName,' AS reverse_cost FROM ways', where_clause);
+      graph_query := concat('SELECT id, source, target, ', rcostName,' AS cost, ', costName,' AS reverse_cost FROM ways', where_clause, ' UNION SELECT * from temp_edges');
     ELSE
-      graph_query := concat('SELECT id, source, target, ', costName,' AS cost, ', rcostName,' AS reverse_cost FROM ways', where_clause);
+      graph_query := concat('SELECT id, source, target, ', costName,' AS cost, ', rcostName,' AS reverse_cost FROM ways', where_clause, ' UNION SELECT * from temp_edges');
     END IF;
 
     -- Requête intermédiaire, permettant de récupérer les données brutes du calcul de l'isochrone.
-    isochrone_query := concat('SELECT dd.seq AS id, ST_X(v.the_geom) AS x, ST_Y(v.the_geom) AS y FROM pgr_drivingDistance($niv2$', graph_query, '$niv2$, ', locationToVID(location, costname, rcostname, where_clause), ', ', costValue, ') AS dd INNER JOIN ways_vertices_pgr AS v ON dd.node = v.id');
-
+    isochrone_query := concat('SELECT dd.seq AS id, ST_X(v.the_geom) AS x, ST_Y(v.the_geom) AS y FROM pgr_drivingDistance($niv2$', graph_query, '$niv2$, -1, ', costValue, ', true) AS dd INNER JOIN ways_vertices_pgr AS v ON dd.node = v.id');
     -- Requête permettant de générer la géométrie finale à renvoyer.
     final_query := concat('SELECT ST_AsGeoJSON(ST_SetSRID(pgr_pointsAsPolygon($1), 4326))');
 
@@ -74,19 +109,18 @@ CREATE OR REPLACE FUNCTION generateIsochrone(
   BEGIN
     -- Calcul de la valeur du 'buffer' (convertie en degrés)
     IF costColumn LIKE 'cost_m%' THEN
-      buffer_value := (costValue + 10) / 112000;
+      buffer_value := (costValue + 100) / 112000::float;
     ELSIF costColumn LIKE 'cost_s%' THEN
       -- Buffer de temps * 130 km/h
-      buffer_value := costValue * (130 / 3.6) / 112000;
+      buffer_value := costValue * (130 / 3.6) / 112000::float;
     ELSE
       buffer_value := 1;
     END IF;
-    where_clause := concat(' WHERE the_geom && (SELECT ST_Expand( ST_Extent( coordToGeom($niv3$', location, '$niv3$)),', buffer_value ,'))');
+    where_clause := concat(' WHERE ways.the_geom && (SELECT ST_Expand( ST_Extent( coordToGeom($niv3$', location, '$niv3$)),', buffer_value ,'))');
     IF constraints != ''
     THEN
       where_clause := concat(where_clause, ' AND ', constraints);
     END IF;
-
     RETURN QUERY SELECT * FROM isochroneGenerator(location, costValue, direction, costColumn, rcostColumn, where_clause);
   END;
 $$ LANGUAGE 'plpgsql' ;
--- sql/utilities.sql
+++ sql/utilities.sql
@@ -14,33 +14,11 @@ CREATE OR REPLACE FUNCTION nearest_edge(lon double precision,
       FROM ways
       ', where_clause, ' AND (', costname, ' > 0 OR ', rcostname, ' > 0)
       ORDER BY the_geom <-> st_setsrid(st_makepoint(',lon,',',lat,'),4326)
-      LIMIT 1 ') ;
+      LIMIT 1; ') ;
     EXECUTE final_query INTO result ;
     RETURN result ;
   END ;
 $$ LANGUAGE 'plpgsql' ;
-
--- Noeud du graphe le plus proche d'un couple de cordonnées
-CREATE OR REPLACE FUNCTION nearest_node(lon double precision,
-                                        lat double precision,
-                                        costname text,         -- nom de la colonne du coût
-                                        rcostname text,        -- nom de la colonne de coût inverse
-                                        where_clause text
-                                        )
-  RETURNS integer AS $$
-  DECLARE
-    result integer;
-    final_query text;
-  BEGIN
-    final_query := concat('SELECT source::integer
-      FROM ways
-      ', where_clause, ' AND (', costname, ' > 0 OR ', rcostname, ' > 0)
-      ORDER BY the_geom <-> st_setsrid(st_makepoint(',lon,',',lat,'),4326)
-      LIMIT 1 ') ;
-    EXECUTE final_query INTO result ;
-    RETURN result ;
-  END ;
-$$ LANGUAGE 'plpgsql';
 
 -- nettoie le graphe : donne un coût négatif aux arc isolés par rapport à la composante connexe principale
 CREATE OR REPLACE FUNCTION clean_graph(
