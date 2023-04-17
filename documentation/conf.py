9c347b8afab3ae3d158bea5207b4c5971e33b25e
Loic <loic.grondin@ign.fr>
2023-04-17 13:41:37 +0200

    [fix] update the doc ref to point on master branch

--- documentation/conf.py
+++ documentation/conf.py
@@ -85,7 +85,7 @@ myst_substitutions = {
     "date_update": datetime.now().strftime("%d %B %Y"),
     "description": "SQL procedures usable with PgRouting to calculate routes and isochrones easily.",
     "repo_url": "https://github.com/IGNF/pgrouting-procedures/",
-    "repo_branch": "develop",
+    "repo_branch": "master",
     "title": html_title,
     "version": "2.0.0",
     "license": "GNU General Public License v3.0",
